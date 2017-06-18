aad6bdc84fb7fd39f8c5a2f8ba537a33c399897f
Sangeet Parashar <sparasha@uwo.ca>
2017-06-18 19:56:41 -0400

    add newMessage and createMessage Events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+        var socket = io(); //this method is used to initiate the client connect to open up to the server . We need to store that in a variable
+        socket.on('connect', function () {
+            console.log('Connected to server');
+
+            //    socket.emit('creatEmail', {
+            //        to: 'shay@gmail.com',
+            //        text: 'sangeet you mean the world to me, be my bf'
+
+            //    });
+            //});
+
+            //socket.on('disconnect', function() {
+            //    console.log('Disconnected from the server');
+            //});
+
+            socket.emit('createMessage', {
+                from: 'shay',
+                text: 'I would love if you could make your app work'
+            });
+        });
+        socket.on('newMessage', function (message) {
+            console.log(JSON.stringify(message, undefined, 2));
+        });
+socket.on('newEmail', function (result) { //the data that is emmitted is a data that can be the first argument in the callback
+    console.log("New email.", JSON.stringify(result, undefined, 2));
+});
+
--- server/server.js
+++ server/server.js
@@ -10,15 +10,35 @@ var server = http.createServer(app);
 
 var io = socketIO(server); //when you set up IO, you have an route that can accept the information and you also have access to javascript which is avaliable at: localhost:3000/socket.io/socket.io.js
 
-//we are simply playing around with the event listeners to see what they exactly do
+////we are simply playing around with the event listeners to see what they exactly do
 io.on('connection', (socket) => { //this socket represents the individual socket rather than all the sockets on the website
     console.log(`New user connected!`);
 
-    socket.on('disconnect', () => {
-        console.log("the client has disconnected! :( ");
-    });
-});
+    //    socket.on('disconnect', () => {
+    //        console.log("the client has disconnected! :( ");
+    //    });
 
+    //    socket.on('creatEmail', function (newEmail) {
+    //        console.log('createEmail',newEmail);
+    //    });
+    //    socket.emit('newEmail', {
+    //        from: 'mike@example.com',
+    //        text: "HEY! I miss you, what's going on",
+    //        createdAt: 123
+    //    }); //by default we don't need to emit any data, the second argument is most commonly is your object that you want to sent
+    //});
+
+    socket.emit('newMessage', {
+        from: 'Sangeet',
+        text: 'This is my chat app',
+        createAt:  Date.now()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log(JSON.stringify(message, undefined, 2));
+    });
+
+});
 //websockets are persistent networks - meaning that the client and the server keep the communication open as long as they want to
 
 var publicPath = path.join(__dirname, '../public');
