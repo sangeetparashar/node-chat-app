041cb8c46f656f690f9f25a8f49e8cbb05b915e0
Sangeet Parashar <sparasha@uwo.ca>
2017-06-20 11:54:11 -0400

    add test cases for geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
         var from = 'Andrew';
@@ -13,5 +13,23 @@ describe('generateMessage', () => {
         });
     });
 
+
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location ', () => {
+        var from = 'deb';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/maps?q=15,19';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({
+            from,
+            url
+            
+        });
+    });
 
 });
